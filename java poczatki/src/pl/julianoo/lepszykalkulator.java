e662cdbca8b48f4dd58cf7d1cb7c16f66b009b06
jjfork <91293626+jjfork@users.noreply.github.com>
2022-04-12 20:04:04 +0200

    Add files via upload

---
+++ java poczatki/src/pl/julianoo/lepszykalkulator.java
@@ -0,0 +1,50 @@
+package pl.julianoo;
+
+import pl.julianoo.Fraction;
+
+import java.util.Scanner;
+
+public class lepszykalkulator {
+    public static void main(String[] args) {
+
+        var scanner = new Scanner(System.in);
+
+        System.out.println("(IF USE FRACTIONS IN THIS SYSTEM X / Y. SPACEBAR IS NESSESARY) Give number in first line, then +.-.*,/ then in next line next number");
+        String runAgain;
+        do {
+
+            var numerator = scanner.nextInt();
+            scanner.next();
+            var denominator = scanner.nextInt();
+            scanner.nextLine();
+            var x = new Fraction(numerator, denominator);
+
+            var operation = scanner.nextLine();
+
+            numerator = scanner.nextInt();
+            scanner.next();
+            denominator = scanner.nextInt();
+            scanner.nextLine();
+            var y = new Fraction(numerator, denominator);
+
+            Fraction result;
+            if (operation.equals("+")) {
+                result = x.add(y);
+            } else if (operation.equals("-")) {
+                result = x.subtract(y);
+            } else if (operation.equals("*")) {
+                result = x.multiply(y);
+            } else if (operation.equals("/")) {
+                result = x.divide(y);
+            } else {
+                System.out.println("wtf");
+                result = new Fraction(0);
+            }
+            System.out.println("Result: " + result.getFractionAsDecimal());
+
+            System.out.println("Your want more? Press y or n.");
+            runAgain = scanner.nextLine();
+        } while (runAgain.equals("y"));
+        scanner.close();
+    }
+}
---
+++ java poczatki/src/pl/julianoo/liczonko.java
@@ -0,0 +1,39 @@
+package pl.julianoo;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class liczonko {
+
+    public static void main(String[] args) {
+
+        var scanner = new Scanner(System.in);
+
+        System.out.println("lets calc, use number then +,-,*,/,% and next number");
+        String runAgain;
+        do {
+            var x = scanner.nextDouble();
+            scanner.nextLine();
+            var operation = scanner.nextLine();
+            var y = scanner.nextDouble();
+            scanner.nextLine();
+            double result;
+            switch (operation) {
+                case "+" -> result = x + y;
+                case "-" -> result = x - y;
+                case "*" -> result = x * y;
+                case "/" -> result = x / y;
+                case "%" -> result = x % y;
+                default -> {
+                    System.out.println("wtf are you doing");
+                    result = 0;
+                }
+            }
+            System.out.println("thats:" + result);
+            System.out.println("you want more? press yes or no");
+            runAgain = scanner.nextLine();
+        } while (Objects.equals(runAgain, "yes"));
+        scanner.close();
+    }
+
+}
